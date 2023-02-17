080758d3b061717f31dff361711154073650741e
K11S11K <125579027+K11S11K@users.noreply.github.com>
2023-02-17 09:27:36 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -22,11 +22,14 @@ async def on_message(message):
     if message.content == f'{PREFIX}call':
         await message.channel.send("callback!")
 
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith(f'{PREFIX}안녕'):
+        await message.channel.send('안녕하세요 첨단 기술로 만들어진 웅봇입니다!')
         
-    if message.content.startswith(f'{PREFIX}random'):
-        await message.channel.send(str(random.randrange(1,100)))
+    if message.content.startswith(f'{PREFIX}ksk'):
+        await message.channel.send('저를 로봇화 시켜준 분입니다!')
+        
+    if message.content.startswith(f'{PREFIX}루한'):
+        await message.channel.send('저의 주인입니다!')
 
 
 try:
